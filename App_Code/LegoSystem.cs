19723e2f029a7fa540268ddb888e4f38cdfffff2
Ka Chun Yung <kcyung780@hotmail.com>
2019-02-27 14:29:00 -0700

    Added comments to each method in Lego class

--- App_Code/LegoSystem.cs
+++ App_Code/LegoSystem.cs
@@ -8,6 +8,7 @@ namespace FalaBricks.LegoSystem.Domain
 {
     public class LegoSystem
     {
+        // Adds a post to the database
         public bool CreatePost(string userName, DateTime postDate, string title, string postText,
             bool isMain, int mainReferencePostID, bool ContainsImage)
         {
@@ -17,6 +18,7 @@ namespace FalaBricks.LegoSystem.Domain
             return Success;
         }
 
+        // Adds an image to the database
         public bool AddImage(int postID, string imagePath)
         {
             PostManager Manager = new PostManager();
@@ -24,30 +26,35 @@ namespace FalaBricks.LegoSystem.Domain
             return Success;
         }
 
+        // Pulls all the main forum posts from the database
         public List<Post> FindMainForumPost()
         {
             PostManager Manager = new PostManager();
             return Manager.GetMainPost();
         }
 
+        // Pulls all main forums posts for a given page number from the database
         public List<Post> FindMainForumPostByPage(int pageNumber)
         {
             PostManager Manager = new PostManager();
             return Manager.GetMainPostByPage(pageNumber);
         }
 
+        // Gets all the images associated with one post from the database
         public List<Image> FindImagesByPostID(int postID)
         {
             PostManager Manager = new PostManager();
             return Manager.GetImagesByPostID(postID);
         }
 
+        // Gets all thread posts for one main post ID from the database
         public List<Post> FindThreadPostByMainPostReference(int mainPostReferenceID)
         {
             PostManager Manager = new PostManager();
             return Manager.GetThreadByMainPostReference(mainPostReferenceID);
         }
 
+        // Changes the UpCounter for a post ID to the database
         public bool ModifyUpCount(int postID, int upCount)
         {
             PostManager Manager = new PostManager();
@@ -55,6 +62,7 @@ namespace FalaBricks.LegoSystem.Domain
             return Success;
         }
 
+        // Changes the DownCounter for a postID to the database
         public bool ModifyDownCount(int postID, int upCount)
         {
             PostManager Manager = new PostManager();
